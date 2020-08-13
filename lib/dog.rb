8cb88cc1fdd551449f4764b2fc0064c7d2034c52
Nancy Jackson <nbjackson23@gmail.com>
2020-08-13 10:06:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,19 @@
 class Dog
-  def initialize(name, breed = "Mut")
-    @name = name
-    @breed = breed
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
-end
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end 
+end 
+  
